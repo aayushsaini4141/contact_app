eae972865825345c558c1e5d88b00c0ab9e90c58
Ayush Saini <aayush.saini4141@gmail.com>
2023-04-10 20:37:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
   const [isErr, setIsErr] = useState(false);
 
   const [contacts, setContacts] = useState([
-    { id: 1, name: "Priyanshu", contact: "7017413590" },
-    { id: 2, name: "Anuj", contact: "9690009778" },
-    { id: 3, name: "Ayush", contact: "8445446569" },
-    { id: 4, name: "John Abrahim", contact: "7017413590" },
-    { id: 5, name: "Salman", contact: "7017413590" },
+    // { id: 1, name: "Priyanshu", contact: "7017413590" },
+    // { id: 2, name: "Anuj", contact: "9690009778" },
+    // { id: 3, name: "Ayush", contact: "8445446569" },
+    // { id: 4, name: "John Abrahim", contact: "7017413590" },
+    // { id: 5, name: "Salman", contact: "7017413590" },
   ]);
 
   const addNewContact = (e) => {
@@ -33,10 +33,7 @@ function App() {
       setContacts(updatedContacts);
       setIsEditing(false);
     } else {
-      //add the logic here
-      //find index of contact==input contact
-      //if(index !== -1) return
-      //else setContact chala de 
+      
       let findIndex = contacts.findIndex((con) => con.contact === contact);
       console.log(findIndex);
       if(findIndex !== -1){
@@ -82,47 +79,47 @@ function App() {
         className="m-4 border p-4 rounded bg-dark text-light"
         onSubmit={addNewContact}
       >
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
             Name
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             onChange={(e) => setName(e.target.value)}
             value={name}
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
             Contact Number
           </label>
           <input
             type="number"
-            class="form-control"
+            className="form-control"
             id="exampleInputPassword1"
             onChange={(e) => setContact(e.target.value)}
             value={contact}
           />
-          {isErr && <p id="emailHelp" class="form-text text-danger fs-6 font-italic">
+          {isErr && <p id="emailHelp" className="form-text text-danger fs-6 font-italic">
             Contact Number should not be duplicate
           </p>}
         </div>
 
-        <button type="submit" class="btn btn-success">
+        <button type="submit" className="btn btn-success">
           {isEditing ? "Update Contact" : "Add Contact"}
         </button>
       </form>
 
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">
+      <div className="mb-3">
+        <label for="exampleInputEmail1" className="form-label">
           Search Contact
         </label>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
           onChange={handleSearch}
@@ -131,7 +128,7 @@ function App() {
         />
       </div>
 
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">#</th>
@@ -150,14 +147,14 @@ function App() {
                 <td>
                   <button
                     type="button"
-                    class="btn btn-sm mx-2 btn-outline-dark"
+                    className="btn btn-sm mx-2 btn-outline-dark"
                     onClick={() => editContact(contact)}
                   >
                     Edit
                   </button>
                   <button
                     type="button"
-                    class="btn btn-sm btn-outline-danger"
+                    className="btn btn-sm btn-outline-danger"
                     onClick={() => deleteContact(contact)}
                   >
                     Delete
